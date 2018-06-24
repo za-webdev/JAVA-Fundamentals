0f7490fb563060d1d5ea86689e730d1f901f5f50
za-webdev <zoyaali22192@gmail.com>
2018-06-24 00:36:14 -0400

    first-commit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+
+//My very first Java Program that will print the folllowing to the console by running the bytecode.
+public class HelloWorld{
+	public static void main(String[] args) {
+		System.out.println("My name is Coding Dojo");
+		System.out.println("I am 100 years old");
+		System.out.println("My home town is  Burbank , CA");
+	}
+}
